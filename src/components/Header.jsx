e1e51c46f5989a88c8eadd5addd22422da18fe18
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-04-03 23:00:52 +0200

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
 import logo from "./../assets/images/logo.png";
+import { HiMoon, HiOutlineMagnifyingGlass, HiSun } from "react-icons/hi2";
 
 export default function Header() {
+  const [toggle, setToggle] = useState(true);
+
   return (
-    <div>
-      <img src={logo} alt="Game Logo" />
+    <div className="flex items-center p-3">
+      <img src={logo} alt="Game Logo" width={90} height={90} />
+      <div className="flex items-center bg-slate-200 mx-5 p-2 rounded-full w-full">
+        <HiOutlineMagnifyingGlass />
+        <input
+          type="search"
+          className="bg-transparent px-2 outline-none"
+          placeholder="Search Games"
+        />
+      </div>
+      <div>
+        {toggle ? (
+          <HiMoon className="bg-slate-200 p-1 rounded-full text-[30px] text-black" />
+        ) : (
+          <HiSun className="bg-slate-200 p-1 rounded-full text-[30px] text-black" />
+        )}
+      </div>
     </div>
   );
 }
