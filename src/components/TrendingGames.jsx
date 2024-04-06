1af7daae9926bf8cd4a3f08bdd6da63b1a315df5
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-04-06 16:55:06 +0200

    Games

--- src/components/TrendingGames.jsx
+++ src/components/TrendingGames.jsx
@@ -5,19 +5,22 @@ export default function TrendingGames({ allGames }) {
       <h1 className="mt-4 font-bold text-[30px] dark:text-white dark: white">
         Trending Games
       </h1>
-      <div className="gap-2 grid grid-cols-4 mt-4">
-        {allGames.map((game) => (
-          <div key={game.id} className="relative">
-            <div className="bottom-0 absolute">
-              <h1 className="font-bold text-white text-x">{game.name}</h1>
-            </div>
-            <img
-              src={game.background_image}
-              alt={`${game.name} image`}
-              className="rounded-lg h-64 object-cover"
-            />
-          </div>
-        ))}
+      <div className="gap-2 hidden md:grid md:grid-cols-3 lg:grid-cols-4 mt-4">
+        {allGames.map(
+          (game, index) =>
+            index < 4 && (
+              <div key={game.id}>
+                <img
+                  src={game.background_image}
+                  alt={`${game.name} image`}
+                  className="rounded-t-xl h-64 object-cover"
+                />
+                <div className="bg-slate-500 p-2 rounded-b-lg w-full h-20 text">
+                  <h1 className="font-bold text-white text-x">{game.name}</h1>
+                </div>
+              </div>
+            )
+        )}
       </div>
     </>
   );
