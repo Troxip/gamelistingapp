b9b9415c5c0d80e259fcb1048785cb0eb289c17f
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-04-06 16:06:34 +0200

    Games

--- src/components/TrendingGames.jsx
+++ src/components/TrendingGames.jsx
@@ -1,3 +1,24 @@
-export default function TrendingGames() {
-  return <div></div>;
+export default function TrendingGames({ allGames }) {
+  console.log(allGames);
+  return (
+    <>
+      <h1 className="mt-4 font-bold text-[30px] dark:text-white dark: white">
+        Trending Games
+      </h1>
+      <div className="gap-2 grid grid-cols-4 mt-4">
+        {allGames.map((game) => (
+          <div key={game.id} className="relative">
+            <div className="bottom-0 absolute">
+              <h1 className="font-bold text-white text-x">{game.name}</h1>
+            </div>
+            <img
+              src={game.background_image}
+              alt={`${game.name} image`}
+              className="rounded-lg h-64 object-cover"
+            />
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
